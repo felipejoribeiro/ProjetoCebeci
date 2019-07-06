1fbc1de34e3ae0772cddb3b24274c908dba2a4a3
Unknown <feliperibeiro.ufu@gmail.com>
2019-07-06 16:47:04 -0300

    ...

--- results/genetic_scatering.py
+++ results/genetic_scatering.py
@@ -10,8 +10,10 @@ scater = np.loadtxt("Gene395_071_400_Ce2temperature.txt", dtype='float')
 
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
-ax.scatter(scater[: , 0], scater[: , 1], scater[: , 2])
+for i in range(len(scater[: , 0])):
+    ax.scatter(scater[i , 0], scater[i , 1], scater[i , 2], c=( i / len(scater[: , 0]), 0.0, 1 - i/len(scater[: , 0])) )
 ax.set_xlabel('Prt')
 ax.set_ylabel('Valor cebeci')
 ax.set_zlabel('L2 norm')
+ax.view_init(30, 100)
 plt.show()
